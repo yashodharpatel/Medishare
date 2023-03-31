3c2492369e2440353fed550923b003b04a28f58d
Yashodhar Patel <patelyashodhar012@gmail.com>
2023-03-31 23:38:46 +0530

    firebase setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./CSS/style.css";
+// import "./CSS/responsive.css";
+import { AuthProvider } from "./Contexts/Authcontext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
